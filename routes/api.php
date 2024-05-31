bc6e0e587274e0e6b304d23a1fda4d2cc0541d4a
Aufa Najid <nl2221171@gmail.com>
2024-05-31 12:56:08 +0700

    feat [AN] auth index

--- routes/api.php
+++ routes/api.php
@@ -64,7 +64,7 @@ Route::get("index",[AuthController::class,"index"]);
  // Routes for AuthmitraController
 
 
- Route::put('accept/{id}', [AuthmitraController::class, 'accept']);
+ Route::put('accept/{id}', [AuthmitraController::class, 'accept'])->middleware('auth:sanctum');
  Route::delete('reject/{id}', [AuthmitraController::class, 'reject'])->middleware('auth:sanctum');
  Route::get('/mitra',[AuthmitraController::class, 'index']);
 Route::put('/mitras/{id}', [AuthmitraController::class, 'update'])->middleware('auth:sanctum');
