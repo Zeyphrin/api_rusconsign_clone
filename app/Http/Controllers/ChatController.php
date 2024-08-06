23680bca288c63955c32f2c960f82cbece1529fa
Aufa Najid <nl2221171@gmail.com>
2024-08-06 22:11:59 +0700

    coba

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chat;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    public function index(GetChatRequest $request) : JsonResponse
+    {
+        $data = $request->validated();
+
+        error_log("AKU INDEX");
+
+        $isPrivate = 1;
+        if ($request -> has('is_private')) {
+            $isPrivate = (int) $data['is_private'];
+        }
+
+        $chats = Chat::where('is_private', $isPrivate)
+            ->hasParticipants(auth()->user()->id)
+            ->whereHas('messages')
+            ->with('lastMessage.user', 'participants.user')
+            ->latest('updated_at')
+            ->get();
+
+
+        return $this->success($chats);
+    }
+
+
+    public function store(StoreChatRequest $request) : JsonResponse
+    {
+        $data = $this->preparedStoreData($request);
+
+        error_log("AKU store");
+
+        if ($data['userId'] == $data['otherUserId']) {
+            error_log( $data['userId']);
+            error_log( $data['otherUserId'] );
+            return $this->error('you cannot chat urself lol');
+        }
+
+        $previousChat = $this->getPreviousChat($data['otherUserId']);
+
+        error_log($previousChat);
+
+        if ($previousChat == null) {
+            $chat = Chat::create($data['data']);
+            $chat->participants()->createMany([
+                ['user_id' => $data['userId']],
+                ['user_id' => $data['otherUserId']]
+            ]);
+
+
+
+            $chat->refresh()->load('lastMessage.user', 'participants.user');
+            return $this->success($chat);
+        }
+
+        return $this->success($previousChat->load('lastMessage.user', 'participants.user'));
+    }
+
+    private function getPreviousChat(int $otherUserId) : mixed {
+        $userId = auth()->user()->id;
+
+        return Chat::where('is_private', 1)
+            ->whereHas('participants', function($query) use ($userId){
+                $query->where('user_id', $userId);
+            })
+            ->whereHas('participants', function($query) use ($otherUserId){
+                $query->where('user_id', $otherUserId);
+            })
+            ->first();
+    }
+
+    protected function error($message, $statusCode = 400)
+    {
+        return response()->json(['error' => $message], $statusCode);
+    }
+
+
+    public function preparedStoreData(StoreChatRequest $request) : array
+    {
+        $data = $request->validated();
+        $otherUserId = (int)$data['user_id'];
+        unset($data['user_id']);
+        $data['created_by'] = auth()->user()->id;
+
+
+        return[
+            'otherUserId'=> $otherUserId,
+            'userId'=> auth()->user()->id,
+            'data'=> $data
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id) : JsonResponse
+    {
+        error_log("AKU show");
+
+        try {
+            $chat = Chat::findOrFail($id);
+            $chat->load('lastMessage.user', 'participants.user');
+            return $this->success($chat);
+        } catch (ModelNotFoundException $e) {
+            return $this->error('Chat tidak ditemukan', 404);
+        }
+    }
+}
---
+++ app/Http/Controllers/ChatMessageController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chat;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ChatMessageController extends Controller
+{
+    public function index(GetMessageRequest $request) : JsonResponse
+    {
+        $data= $request->validated();
+        $chatId = $data['chat_id'];
+        $currentPage = $data['page'];
+        $pageSize = $data['page_size'] ?? 15;
+
+        $messages = ChatMessage::where('chat_id', $chatId)
+            ->with('user')
+            ->latest('created_at')
+            ->simplePaginate(
+                $pageSize,
+                ['*'],
+                'page',
+                $currentPage
+
+
+
+            );
+        return $this->success(
+            $messages -> getCollection()
+        );
+    }
+
+    public function store(StoreMessageRequest $request) : JsonResponse{
+        $data = $request->validated();
+        $data['user_id'] = auth()->user()->id;
+
+        $chatMessage = ChatMessage::create($data);
+        $chatMessage->load('user');
+
+        $this->sendNotificationToOther($chatMessage);
+
+        return $this->success($chatMessage, 'pesan sudah terkirim ');
+
+    }
+
+    private function sendNotificationToOther(ChatMessage $chatMessage) : void{
+        $chatId = $chatMessage->chat_id;
+
+        broadcast(new NewMessageSent($chatMessage))->toOthers();
+
+        $user = auth()->user();
+        $userId = $user->id;
+
+        $chat = Chat::where('id', $chatMessage->chat_id)
+            ->with(['participants' =>  function($query) use ($userId){
+                $query->where('user_id', '!=', $userId);
+            }])
+            ->first();
+
+        if ($chat->participants->count() > 0) {
+            $otherUserId = $chat->participants->first()->user_id;
+
+            $otherUser = User::where('id', $otherUserId)->first();
+            $otherUser->sendNowMessageNotification([
+                'messageData' => [
+                    'senderName'=> $user->username,
+                    'message'=> $chatMessage->message,
+                    'chatId'=> $chatMessage->chat_id
+                ]
+            ]);
+
+        }
+    }
+}
